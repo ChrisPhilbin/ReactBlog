d6938fbb58a014d2362c18c4a2c001506f5c6fa3
Chris Philbin <chris@chrisphilbin.net>
2021-04-01 07:05:09 -0400

    Adding SideBar component to Router in order to show on all pages

--- view/src/App.js
+++ view/src/App.js
@@ -4,19 +4,24 @@ import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import Home from './pages/Home'
 import Posts from './components/ShowAllPosts'
 import CreateNewPost from './components/CreateNewPost'
-import ShowOnePost from './components/ShowOnePost';
-import EditOnePost from './components/EditOnePost';
+import ShowOnePost from './components/ShowOnePost'
+import EditOnePost from './components/EditOnePost'
+import SideBar from './navigation/SideBar'
 
 const App = () => {
   return(
     <Router>
-        <Switch>
-          <Route exact path="/" component={Home} />
-          <Route exact path="/posts" component={Posts} />
-          <Route exact path="/posts/new" component={CreateNewPost} />
-          <Route exact path="/posts/:postId" component={ShowOnePost} />
-          <Route exact path="/posts/:postId/edit" component={EditOnePost} />
-        </Switch>
+      <div>
+        <SideBar />
+          <Switch>
+            <Route exact path="/" component={Home} />
+            <Route exact path="/posts" component={Posts} />
+            <Route exact path="/posts/new" component={CreateNewPost} />
+            <Route exact path="/posts/:postId" component={ShowOnePost} />
+            <Route exact path="/posts/:postId/edit" component={EditOnePost} />
+            <SideBar />
+          </Switch>
+      </div>
     </Router>
   )
 }
--- view/src/navigation/SideBar.js
+++ view/src/navigation/SideBar.js
@@ -3,7 +3,8 @@ import { makeStyles } from '@material-ui/core/styles'
 import Drawer from '@material-ui/core/Drawer'
 import List from '@material-ui/core/List'
 import ListItem from '@material-ui/core/ListItem'
-import Divider from '@material-ui/core/Divier'
+import ListItemText from '@material-ui/core/ListItemText'
+import Divider from '@material-ui/core/Divider'
 
 const drawerWidth = 240;
 
