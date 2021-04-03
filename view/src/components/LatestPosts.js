1957924b54e931fd37e574d89744f23597df32ac
Chris Philbin <chris@chrisphilbin.net>
2021-04-02 21:27:09 -0400

    Adding CircularProgress element from material-ui when fetching latest posts

--- view/src/components/LatestPosts.js
+++ view/src/components/LatestPosts.js
@@ -1,4 +1,5 @@
 import React, { useEffect, useState } from 'react'
+import CircularProgress from '@material-ui/core/CircularProgress'
 import { Link } from 'react-router-dom'
 import List from '@material-ui/core/List'
 import ListItem from '@material-ui/core/ListItem'
@@ -7,10 +8,23 @@ import { Typography } from '@material-ui/core'
 import { makeStyles } from '@material-ui/core/styles'
 
 const useStyles = makeStyles({
+    root: {
+		display: 'flex'
+	},
+    postLink: {
+
+    },
     recentPosts: {
         padding: 10,
+        margin: 10,
+    },
+    uiProgess: {
+        display: 'block',
+        marginLeft: 'auto',
+        marginRight: 'auto',
+        padding: 10,
         margin: 10
-    }
+	}
 })
 
 const LatestPosts = () => {
@@ -35,9 +49,9 @@ const LatestPosts = () => {
 
     if (loading) {
         return(
-            <>
-                Loading...
-            </>
+            <div className={classes.root}>
+                {loading && <CircularProgress size={150} className={classes.uiProgess} />}
+            </div>
         )
     } else {
         return(
@@ -45,7 +59,7 @@ const LatestPosts = () => {
             <Typography variant="h4" gutterBottom>Latest Posts</Typography>
                 <List>
                     {recentPosts.map((post) => (
-                        <Link to={"/posts/" + post.postId}>
+                        <Link to={"/posts/" + post.postId} className={classes.postLink}>
                             <ListItem button key={post.postId}>
                                 <ListItemText primary={post.title}/>
                             </ListItem>
