c7ac77b55891c7cde02c1bd29e2f28900cc5c479
Chris Philbin <chris@chrisphilbin.net>
2021-04-10 07:05:18 -0400

    Creating component to display all posts within a specific category

--- view/src/components/category/ShowAllCategoryPosts.js
+++ view/src/components/category/ShowAllCategoryPosts.js
@@ -1,43 +1,62 @@
-import React, { useEffect, useState} from 'react'
+import React, { useEffect, useState } from 'react'
+import CircularProgress from '@material-ui/core/CircularProgress'
+import Container from '@material-ui/core/Container'
+import { makeStyles } from '@material-ui/core/styles'
+import DisplayPost from '../DisplayPost'
+
+const useStyles = makeStyles({
+    root: {
+      width: '75%'
+    },
+    spacer: {
+        height: 100,
+    },
+    toolbarSpacer: {
+        height: 40
+    },
+    uiProgess: {
+        display: 'block',
+        marginLeft: 'auto',
+        marginRight: 'auto',
+        padding: 10,
+        margin: 10
+	}
+})
 
 const ShowAllCategoryPosts = (props) => {
 
-    const categoryName = props.match.params.categoryName
-    let [loading, setLoading] = useState(true)
+    const classes = useStyles()
+
     let [posts, setPosts]     = useState([])
-    let [errors, setErrors]   = useState(false)
+    let [loading, setLoading] = useState(true)
 
     useEffect(() => {
-        fetch(process.env.REACT_APP_CORS + `/categories/${categoryName}`)
-        .then(response => {
-            if (response.status === 200) {
-                response.json()
-                .then(data => setPosts(data))
-                .then(setLoading(false))
-            } else {
-                setErrors(true)
-            }
+        fetch(process.env.REACT_APP_CORS + `/categories/${props.match.params.categoryName}`)
+        .then(response => response.json())
+        .then(data => {
+            setPosts(data)
+            setLoading(false)
         })
-    })
+        .catch(error => console.log(error, "something went wrong"))
+    },[])
 
-    console.log(posts, "posts from category")
-    if (loading) {
-        return (
-            <>
-            Loading...
-            </>
-        )
-    } else if (errors) {
+    if (loading && !posts.length) {
         return(
-            <>
-            Error, something went wrong
-            </>
+            <div className={classes.root}>
+                <CircularProgress size={150} className={classes.uiProgess} />
+            </div>
         )
     } else {
         return(
-            <>
-            Showing all posts within category
-            </>
+            <div className={classes.root}>
+                <div className={classes.toolbarSpacer} />
+                <Container maxWidth="lg">
+                    {posts.map((post) => (
+                        <DisplayPost post={post} />
+                    ))}
+                </Container>
+                <div className={classes.spacer} />
+            </div>
         )
     }
 }
