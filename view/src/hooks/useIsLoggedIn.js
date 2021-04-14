82ab226fa7797515fcb7baef88d5b6b31fb6980a
Chris Philbin <chris@chrisphilbin.net>
2021-04-14 17:00:56 -0400

    Updating package.json

--- view/src/hooks/useIsLoggedIn.js
+++ view/src/hooks/useIsLoggedIn.js
@@ -1,6 +1,6 @@
-export const useIsLoggedIn = (history) => {
-    const authToken = localStorage.getItem('AuthToken');
-    if(authToken === null){
-        history.push('/login')
-    }
-}
+// export const useIsLoggedIn = (history) => {
+//     const authToken = localStorage.getItem('AuthToken');
+//     if(authToken === null){
+//         history.push('/login')
+//     }
+// }
