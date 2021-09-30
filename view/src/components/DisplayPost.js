a19a7ae26eec7ff7b413faa12324fe098d35acf7
Chris Philbin <chris@chrisphilbin.net>
2021-09-30 19:33:50 -0400

    Parsing text as html

--- view/src/components/DisplayPost.js
+++ view/src/components/DisplayPost.js
@@ -1,50 +1,70 @@
-import React from 'react'
-import { Link } from 'react-router-dom'
-import { makeStyles } from '@material-ui/styles'
-import moment from 'moment'
-import Paper from '@material-ui/core/Paper'
-import Typography from '@material-ui/core/Typography'
+import React from "react";
+import { Link } from "react-router-dom";
+import { makeStyles } from "@material-ui/styles";
+import moment from "moment";
+import Paper from "@material-ui/core/Paper";
+import Typography from "@material-ui/core/Typography";
 
 const useStyles = makeStyles({
-    postPaper: {
-        padding: 20,
-        marginBottom: 45,
-        fontFamily: 'arial'
-    },
-    postTitle: {
-        fontWeight: 'bold',
-        textDecoration: 'none',
-        
-    },
-    subTitle: {
-        fontWeight: 'bold'
-    },
-})
+  postPaper: {
+    padding: 20,
+    marginBottom: 45,
+    fontFamily: "arial",
+  },
+  postTitle: {
+    fontWeight: "bold",
+    textDecoration: "none",
+  },
+  subTitle: {
+    fontWeight: "bold",
+  },
+});
 
 const DisplayPost = (props) => {
+  const post = props.post;
 
-    const post = props.post
+  const classes = useStyles();
 
-    const classes = useStyles()
+  const maxLength = 350;
 
-    const maxLength = 350
+  const formatPost = (post) => {
+    let shortenedPost = post.body.substring(0, maxLength);
+    return (
+      <div>
+        <div dangerouslySetInnerHTML={{ __html: shortenedPost }} />{" "}
+        <Link to={`/posts/${post.postId}`}> ...Read more</Link>
+      </div>
+    );
+  };
 
-    const formatPost = (post) => {    
-        let shortenedPost = post.body.substring(0,maxLength)
-        return(
-            <div>
-            {shortenedPost} <Link to={`/posts/${post.postId}`}> ...Read more</Link>
-            </div>
-        )
-    }
+  return (
+    <Paper key={post.postId} className={classes.postPaper} elevation={8}>
+      <Typography variant="h5" gutterBottom className={classes.postTitle}>
+        <Link
+          to={"/posts/" + post.postId}
+          style={{ textDecoration: "none", color: "dodgerBlue" }}
+        >
+          {post.title}
+        </Link>
+      </Typography>
+      {post.category ? (
+        <span className={classes.subTitle}>
+          <em>
+            Posted in{" "}
+            <Link to={"/categories/" + post.category}>{post.category}</Link> -{" "}
+            {moment(post.createdAt).format("LL")}
+            <br />
+            <br />
+          </em>
+        </span>
+      ) : null}
+      {post.body.length >= maxLength ? (
+        formatPost(post)
+      ) : (
+        <div dangerouslySetInnerHTML={{ __html: post.body }} />
+      )}
+    </Paper>
+  );
+};
 
-    return(
-        <Paper key={post.postId} className={classes.postPaper} elevation={8}>
-            <Typography variant="h5" gutterBottom className={classes.postTitle}><Link to={"/posts/" + post.postId} style={{textDecoration: 'none', color: 'dodgerBlue'}}>{post.title}</Link></Typography>
-            {post.category ? <span className={classes.subTitle}><em>Posted in <Link to={"/categories/" + post.category}>{post.category}</Link> - {moment(post.createdAt).format('LL')}<br /><br /></em></span> : null }
-            {post.body.length >= maxLength ? formatPost(post) : post.body}
-        </Paper>
-    )
-}
-
-export default DisplayPost
+export default DisplayPost;
--- view/src/components/ShowOnePost.js
+++ view/src/components/ShowOnePost.js
@@ -1,111 +1,110 @@
-import React, { useEffect, useState } from 'react'
+import React, { useEffect, useState } from "react";
 
-import Container from '@material-ui/core/Container'
-import DeleteForeverIcon from '@material-ui/icons/DeleteForever'
-import IconButton from '@material-ui/core/IconButton'
-import EditIcon from '@material-ui/icons/Edit'
-import { makeStyles } from '@material-ui/core/styles'
-import Paper from '@material-ui/core/Paper'
-import Typography from '@material-ui/core/Typography'
-import ShowLoading from './ShowLoading'
-import { useCheckToken } from '../hooks/customHooks'
+import Container from "@material-ui/core/Container";
+import DeleteForeverIcon from "@material-ui/icons/DeleteForever";
+import IconButton from "@material-ui/core/IconButton";
+import EditIcon from "@material-ui/icons/Edit";
+import { makeStyles } from "@material-ui/core/styles";
+import Paper from "@material-ui/core/Paper";
+import Typography from "@material-ui/core/Typography";
+import ShowLoading from "./ShowLoading";
+import { useCheckToken } from "../hooks/customHooks";
 
 const useStyles = makeStyles({
-    postIcons: {
-        position: 'relative',
-        float: 'right',
-        right: 0,
-        top: 0
-    },
-    root: {
-      width: '90%',
-    },
-    postPaper: {
-        position: 'relative',
-        padding: 10,
-        margin: 10
-    }
-})
+  postIcons: {
+    position: "relative",
+    float: "right",
+    right: 0,
+    top: 0,
+  },
+  root: {
+    width: "90%",
+  },
+  postPaper: {
+    position: "relative",
+    padding: 10,
+    margin: 10,
+  },
+});
 
 const ShowOnePost = (props) => {
+  const classes = useStyles();
 
-    const classes = useStyles()
+  const isLoggedIn = useCheckToken();
 
-    const isLoggedIn = useCheckToken()
+  let [post, setPost] = useState({});
+  let [loading, setLoading] = useState(true);
 
-    let [post, setPost] = useState({})
-    let [loading, setLoading] = useState(true)
+  useEffect(() => {
+    fetch(process.env.REACT_APP_CORS + `/posts/${props.match.params.postId}`)
+      .then((response) => response.json())
+      .then((data) => {
+        setPost(data);
+        setLoading(false);
+        document.title = post.title;
+      });
+  }, []);
 
-    useEffect(() => {
-        fetch(process.env.REACT_APP_CORS + `/posts/${props.match.params.postId}`)
-        .then(response => response.json())
-        .then(data => {
-            setPost(data)
-            setLoading(false)
-            document.title = post.title
-        })
-    },[])
+  const handleDelete = () => {
+    if (window.confirm("Are you sure?")) {
+      let deletedPost = {
+        postId: props.match.params.postId,
+      };
 
-    const handleDelete = () => {
-        if (window.confirm("Are you sure?")) {
-
-            let deletedPost = {
-                postId: props.match.params.postId
-            }
-
-            fetch(process.env.REACT_APP_CORS + `/posts/${props.match.params.postId}`, {
-                method: 'delete',
-                body: JSON.stringify(deletedPost),
-                headers: {
-                    'Content-type': 'application/json'
-                }
-            })
-            .then(response => {
-                if (response.status === 200) {
-                    alert("Deleted!")
-                    props.history.push('/posts')
-                }
-            })
+      fetch(
+        process.env.REACT_APP_CORS + `/posts/${props.match.params.postId}`,
+        {
+          method: "delete",
+          body: JSON.stringify(deletedPost),
+          headers: {
+            "Content-type": "application/json",
+          },
         }
+      ).then((response) => {
+        if (response.status === 200) {
+          alert("Deleted!");
+          props.history.push("/posts");
+        }
+      });
     }
+  };
 
-    if (loading) {
-        return(
-            <ShowLoading />
-        )
-    } else {
-        return(
-            <div className={classes.root}>
-                <Container maxWidth="lg">
-                    <Paper className={classes.postPaper} elevation={3}>
-                        { isLoggedIn ? 
-                            <div className={classes.postIcons}>
-                                <IconButton
-                                    color="primary"
-                                    aria-label="Add a new reply"
-                                    href={`/posts/${props.match.params.postId}/edit`}
-                                >
-                                    <EditIcon style={{ fontSize: 30 }} />
-                                </IconButton>
+  if (loading) {
+    return <ShowLoading />;
+  } else {
+    return (
+      <div className={classes.root}>
+        <Container maxWidth="lg">
+          <Paper className={classes.postPaper} elevation={3}>
+            {isLoggedIn ? (
+              <div className={classes.postIcons}>
+                <IconButton
+                  color="primary"
+                  aria-label="Add a new reply"
+                  href={`/posts/${props.match.params.postId}/edit`}
+                >
+                  <EditIcon style={{ fontSize: 30 }} />
+                </IconButton>
 
-                                <IconButton
-                                    color="secondary"
-                                    aria-label="Add a new reply"
-                                    onClick={handleDelete}
-                                >
-                                    <DeleteForeverIcon style={{ fontSize: 30 }} />
-                                </IconButton>
-                            </div>
-                        :
-                            null
-                        }
-                        <Typography variant="h3" gutterBottom>{post.title}<br /></Typography>
-                        {post.body}
-                    </Paper>
-                </Container>
-            </div>
-        )
-    }
-}
+                <IconButton
+                  color="secondary"
+                  aria-label="Add a new reply"
+                  onClick={handleDelete}
+                >
+                  <DeleteForeverIcon style={{ fontSize: 30 }} />
+                </IconButton>
+              </div>
+            ) : null}
+            <Typography variant="h3" gutterBottom>
+              {post.title}
+              <br />
+            </Typography>
+            <div dangerouslySetInnerHTML={{ __html: post.body }} />
+          </Paper>
+        </Container>
+      </div>
+    );
+  }
+};
 
-export default ShowOnePost
+export default ShowOnePost;
