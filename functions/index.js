4a56c3a19d09feabfd1e956248183d0709331494
Chris Philbin <chris@chrisphilbin.net>
2021-10-01 08:05:19 -0400

    Updating API end points

--- functions/index.js
+++ functions/index.js
@@ -1,48 +1,46 @@
-const functions = require('firebase-functions');
-const app = require('express')();
-const auth = require('./util/auth');
+const functions = require("firebase-functions");
+const app = require("express")();
+const auth = require("./util/auth");
 
 const {
-    createOnePost,
-    deleteOneReply,
-    editOnePost,
-    getAllPosts,
-    getOnePost,
-    getLatestPosts
-} = require('./api/posts')
+  createOnePost,
+  deleteOnePost,
+  editOnePost,
+  getAllPosts,
+  getOnePost,
+  getLatestPosts,
+} = require("./api/posts");
 
-app.post('/posts', auth, createOnePost);
-app.get('/posts', getAllPosts);
-app.get('/posts/latest', getLatestPosts);
-app.get('/posts/:postId', getOnePost);
-app.put('/posts/:postId', auth, editOnePost);
-app.delete('/posts/:postId', auth, deleteOneReply);
+app.post("/posts", auth, createOnePost);
+app.get("/posts", getAllPosts);
+app.get("/posts/latest", getLatestPosts);
+app.get("/posts/:postId", getOnePost);
+app.put("/posts/:postId", auth, editOnePost);
+app.delete("/posts/:postId", auth, deleteOnePost);
 exports.api = functions.https.onRequest(app);
 
 const {
-    createOneCategory,
-    getAllCategories,
-    getAllPostsInCategory
-} = require('./api/categories')
+  createOneCategory,
+  getAllCategories,
+  getAllPostsInCategory,
+} = require("./api/categories");
 
-app.get('/categories', getAllCategories);
-app.post('/categories', auth, createOneCategory);
-app.get('/categories/:categoryName', getAllPostsInCategory)
-
-const {
-    createOneStaticPage,
-    getOneStaticPage
-} = require('./api/static')
-
-app.get('/static/:pageName', getOneStaticPage)
-app.post('/static', auth, createOneStaticPage)
+app.get("/categories", getAllCategories);
+app.post("/categories", auth, createOneCategory);
+app.get("/categories/:categoryName", getAllPostsInCategory);
 
 const {
-    loginUser,
-    getUserDetail,
-    isUserSignedIn
-} = require('./api/users')
+  createOneStaticPage,
+  getOneStaticPage,
+  getAllStaticPages,
+} = require("./api/static");
 
-app.post('/login', loginUser)
-app.get('/user', auth, getUserDetail)
-app.post('/user/auth', isUserSignedIn)
+app.get("/static/:pageName", getOneStaticPage);
+app.get("/static", getAllStaticPages);
+app.post("/static", auth, createOneStaticPage);
+
+const { loginUser, getUserDetail, isUserSignedIn } = require("./api/users");
+
+app.post("/login", loginUser);
+app.get("/user", auth, getUserDetail);
+app.post("/user/auth", isUserSignedIn);
